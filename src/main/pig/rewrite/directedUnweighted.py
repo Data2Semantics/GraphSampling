351b82c8f7211afc05e8e603604edc7c85b28269
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-02-20 16:16:37 +0100

    added sample parameter to rewrite script

--- src/main/pig/rewrite/directedUnweighted.py
+++ src/main/pig/rewrite/directedUnweighted.py
@@ -19,16 +19,16 @@ DEFINE UnorderedPairs datafu.pig.bags.UnorderedPairs();
 REGISTER lib/d2s4pig-1.0.jar;
 DEFINE NtLoader com.data2semantics.pig.loaders.NtLoader();
 DEFINE LONGHASH com.data2semantics.pig.udfs.LongHash();
-
-rdfGraph = LOAD '$inputFile' USING NtLoader() AS (sub:chararray, pred:chararray, obj:chararray);
---rdfGraph = SAMPLE largeGraph 0.0000001; --0.0000001: 76 items
---rdfGraph = SAMPLE largeGraph 0.001; --0.0001: 75745 items
---dump rdfGraph;
-
-/**
- * CREATING A NETWORK OF S->O NODES (directed, unlabelled)
- */
 """
+
+if sample == 1:
+    """rdfGraph = LOAD '$inputFile' USING NtLoader() AS (sub:chararray, pred:chararray, obj:chararray);
+    """
+else:
+    """inputGraph = LOAD '$inputFile' USING NtLoader() AS (sub:chararray, pred:chararray, obj:chararray);
+    rdfGraph = SAMPLE largeGraph $sample;
+    """
+
 
 if useLongHash:
     pigScript += """rewrittenGraph = FOREACH rdfGraph GENERATE LONGHASH(sub), 1, LONGHASH(obj) ; --just use a weight of 1 for now
