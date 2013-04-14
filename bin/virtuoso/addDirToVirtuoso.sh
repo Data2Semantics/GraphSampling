5dc453aaa858b65ec55afa545d9a1eb655fa9740
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-04-14 21:07:57 +0200

    improved args for adding dir to virtuoso

--- bin/virtuoso/addDirToVirtuoso.sh
+++ bin/virtuoso/addDirToVirtuoso.sh
@@ -5,20 +5,22 @@ if [ -z "$1" ];then
 fi
 source /home/OpenPHACTS-Virtuoso/virtuoso-environment.sh;
 isqlFile="${HOME}/.isqlCmdFile.sql"
-dir=$(readlink -f $1)
 
-basename=`basename $dir`
-graphname="http://$basename"
-
-echo "Adding $dir to load list"
-echo "ld_dir('$dir','*.nt','$graphname');" > $isqlFile;
-echo "EXIT;" >> $isqlFile;
-echo "" >> $isqlFile;
-cat $isqlFile | isql;
-
-echo "Running loader"
-echo "rdf_loader_run();&" > $isqlFile;
-echo "EXIT;" >> $isqlFile;
-echo "" >> $isqlFile;
-cat $isqlFile | isql;
+for dir in "$@"; do
+	absDir=$(readlink -f $dir)
+	basename=`basename $absDir`
+	graphname="http://$basename"
+	
+	echo "Adding $dir to load list"
+	echo "ld_dir('$absDir','*.nt','$graphname');" > $isqlFile;
+	echo "EXIT;" >> $isqlFile;
+	echo "" >> $isqlFile;
+	cat $isqlFile | isql;
+	
+	echo "Running loader"
+	echo "rdf_loader_run();&" > $isqlFile;
+	echo "EXIT;" >> $isqlFile;
+	echo "" >> $isqlFile;
+	cat $isqlFile | isql;
+done
 
