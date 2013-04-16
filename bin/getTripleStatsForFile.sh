cf4c086aeb0e13d518fbdb69bd5fb3acfdffba8a
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-04-16 23:54:12 +0200

    improved args for this script

--- bin/getTripleStatsForFile.sh
+++ bin/getTripleStatsForFile.sh
@@ -4,18 +4,23 @@ if [ -z "$1" ];then
         exit;
 fi
 
-tripleWeightsFile=$1
 rDir="${HOME}/rProject"
 plotsDir="$HOME/stats/plots/tripleWeightDist"
 scriptsFile="$HOME/rProject/scripts/getTripleStats.R"
-outputRunScript="$HOME/.rRunScript.R"
+outputRunScript="$HOME/temp/rRunScript.R"
 outputRunScript+=`date +%s%N`
-basename=`basename $tripleWeightsFile`
-targetFile="$plotsDir/$basename.pdf"
-echo "filename <- \"$tripleWeightsFile\"" > $outputRunScript;
-echo "outputPdf <- \"$targetFile\"" >> $outputRunScript;
-cat $scriptsFile >> $outputRunScript;
-R -f $outputRunScript &
+logFile="$HOME/logs/getTripleStats_"
+logFile+=`date +"%Y%m%d"`
+logFile+=".log"
+
+for tripleWeightsFile in "$@"; do
+	basename=`basename $tripleWeightsFile`
+	targetFile="$plotsDir/$basename.pdf"
+	echo "filename <- \"$tripleWeightsFile\"" > $outputRunScript;
+	echo "outputPdf <- \"$targetFile\"" >> $outputRunScript;
+	cat $scriptsFile >> $outputRunScript;
+	R -f $outputRunScript > $logFile &
+done
 
 
 
