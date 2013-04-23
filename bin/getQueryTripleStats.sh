e23d148ab2c032f8dfa2ae2d9071e863fc027679
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-04-23 16:42:29 +0200

    added script to get query triple stats

---
+++ bin/getQueryTripleStats.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+function hadoopLs {
+	hadoopLs=()
+	echo "hadoop fs -ls $1";
+	dirListing=`hadoop fs -ls $1`;
+	for word in ${dirListing} ; do
+ 		if [[ $word =~ ^/ ]];then 
+	    	hadoopLs+=(${word})
+	    fi
+	done
+}  
+
+if [ -z "$1" ];then
+        echo "at least 1 argument required (rewrite methods as directories to get query triples stats for)"
+        exit;
+fi
+
+
+
+
+
+
+for dir in "$@"; do
+	dirBasename=`basename $dir`
+	IFS=_ read -a delimited <<< "$dir"
+	dataset=${delimited[0]}
+	hadoopLs $dataset/queryStatsInput/
+	if [ ${#hadoopLs[@]} == "0" ]; then
+		echo "Did not find query files to get stats for. ($dataset/queryStatsInput/)"
+		exit;
+	fi
+	echo "running pig to get weights for query triples $dirBasename"
+	for queryFile in "${hadoopLs[@]}"; do
+		pig pigAnalysis/rewrite/getQueryTripleWeights.py $dataset/roundtrip/$dirBasename $queryFile;
+	done
+done
