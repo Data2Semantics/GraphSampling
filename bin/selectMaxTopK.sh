6cc5053743699b869b002580eb3bb5d93db062d7
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-04-23 17:43:07 +0200

    another helper script

---
+++ bin/selectMaxTopK.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+function hadoopLs {
+        hadoopLs=()
+        echo "hadoop fs -ls $1";
+        dirListing=`hadoop fs -ls $1`;
+        for word in ${dirListing} ; do
+                if [[ $word =~ ^/ ]];then 
+                hadoopLs+=(${word})
+            fi
+        done
+}  
+
+
+if [ -z "$1" ];then
+        echo "at least 1 argument required (directory path for roundtrips)"
+        exit;
+fi
+
+
+for dir in "$@"; do
+        echo "selecting topk for $dir";
+        analysisFiles=`find $dir/output/*`
+        while read -r analysisFile; do
+                selectMaxTopKForAnalysisFile.sh $analysisFile; 
+        done <<< "$analysisFiles"
+done
+
